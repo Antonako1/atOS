7468df85d0a779e6475d1f99b2a6beab3fc1de6c
antonakonpelto <anton.akonpelto@gmail.com>
2023-04-30 23:17:13 +0300

    1

--- src/remove.js
+++ src/remove.js
@@ -4,13 +4,15 @@ remove = function(cmd){
     if(cmd === "root" || cmd === "root/"){
         return "Bruhh"
     }else{
-        let tulokset = deleteOBJ(cmd, data)
+        let tulokset = deleteOBJ(cmd, global.data)
         fs.writeFileSync('files/file.json', JSON.stringify(global.data, null, 2));
         return tulokset;
     }
 }
-
+// TODO korjaa
 function deleteOBJ(name, location) {
+    console.log('location::: ', location);
+    console.log('name::: ', name);
     for (let i in location.filedata) {
         let item = location.filedata[i];
         if (item && item.name == name) {
