b84ca5b0f938cd04b6224d68b5b3fb9c4b41ca26
Antonako1 <114558368+Antonako1@users.noreply.github.com>
2025-10-21 11:52:51 +0300

    Refactored commands

--- SOURCE/PROGRAMS/SHELL/COMMANDS.h
+++ SOURCE/PROGRAMS/SHELL/COMMANDS.h
@@ -7,4 +7,24 @@
 
 U0 HANDLE_COMMAND(U8 *line);
 
+typedef VOID (*ShellCommandHandler)(U8 *line);
+
+// Command entry structure
+typedef struct {
+    const CHAR *name;
+    ShellCommandHandler handler;
+    const CHAR *help;
+} ShellCommand;
+
+// === Command function prototypes ===
+VOID CMD_HELP(U8 *line);
+VOID CMD_CLEAR(U8 *line);
+VOID CMD_VERSION(U8 *line);
+VOID CMD_EXIT(U8 *line);
+VOID CMD_ECHO(U8 *line);
+VOID CMD_TONE(U8 *line);
+VOID CMD_SOUNDOFF(U8 *line);
+VOID CMD_UNKNOWN(U8 *line);
+
+
 #endif // COMMANDS_H
