4e4583b878593a6cab1cadf357a847ccbc3a70db
Antonako1 <114558368+Antonako1@users.noreply.github.com>
2025-11-02 15:49:42 +0200

    Skeleton for ironclad

---
+++ SOURCE/PROGRAMS/IRONCLAD/IRONCLAD.c
@@ -0,0 +1,10 @@
+#include <STD/RUNTIME.h>
+#include "IRONCLAD.h"
+
+U32 IRONCLAD_LOOP() {
+
+}
+
+U32 main(U32 argc, PPU8 argv) {
+    return IRONCLAD_LOOP();
+}
---
+++ SOURCE/PROGRAMS/IRONCLAD/IRONCLAD.h
@@ -0,0 +1,10 @@
+#ifndef IRONCLAD_H
+#define IRONCLAD_H
+
+#include <STD/TYPEDEF.h>
+
+typedef struct _IRONCLAD {
+
+} IRONCLAD, *PIRONCLAD;
+
+#endif // IRONCLAD_H
