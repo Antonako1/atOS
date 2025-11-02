6046f8b46b686cce3468df456224fbd0be70046a
Antonako1 <114558368+Antonako1@users.noreply.github.com>
2025-11-02 15:49:27 +0200

    Create ARG.h

---
+++ SOURCE/STD/ARG.h
@@ -0,0 +1,19 @@
+#ifndef STDARG_H
+#define STDARG_H
+
+// 'va_list' is a pointer type used to access variable arguments
+typedef unsigned char *va_list;
+
+// Align 't' to the next multiple of 'sizeof(int)'
+#define _VA_ALIGN(t)    (((sizeof(t) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
+
+// Initialize 'ap' to point to the first variable argument
+#define va_start(ap, last_arg)  (ap = (va_list)&(last_arg) + _VA_ALIGN(last_arg))
+
+// Get the next argument value of type 't'
+#define va_arg(ap, t)  (*(t *)((ap += _VA_ALIGN(t)) - _VA_ALIGN(t)))
+
+// End traversal of arguments (nothing to do here)
+#define va_end(ap)     (ap = (va_list)0)
+
+#endif // STDARG_H
