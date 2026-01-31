fdffbb3f8fe2c5296ee068d211655a9da85ba866
Antonako1 <114558368+Antonako1@users.noreply.github.com>
2026-01-31 22:27:42 +0200

    Added KDEBUG logs to RTOSKRNL

--- SOURCE/KERNEL/RTOSKRNL.c
+++ SOURCE/KERNEL/RTOSKRNL.c
@@ -11,24 +11,32 @@ void rtos_kernel(U0) {
     debug_vram_start(); // Start in early mode, using direct framebuffer access
     // These are called here, to fix address issues and to set up new values
     GDT_INIT();
+    KDEBUG_PUTS("[atOS] GDT OK\n");
     IDT_INIT();
+    KDEBUG_PUTS("[atOS] IDT OK\n");
     IRQ_INIT();
+    KDEBUG_PUTS("[atOS] IRQ OK\n");
     SETUP_ISR_HANDLERS();
+    KDEBUG_PUTS("[atOS] ISR OK\n");
     fpu_enable();
+    KDEBUG_PUTS("[atOS] FPU OK\n");
+
     panic_if(!vesa_check(), PANIC_TEXT("Failed to initialize VESA"), PANIC_INITIALIZATION_FAILED);
-    KDEBUG_PUTS("[atOS] VESA/VBE OK\n");
-    
     panic_if(!vbe_check(), PANIC_TEXT("Failed to initialize VBE"), PANIC_INITIALIZATION_FAILED);
+    
+    KDEBUG_PUTS("[atOS] VESA/VBE OK\n");
 
     panic_if(INITIALIZE_ATAPI() == ATA_FAILED, PANIC_TEXT("Failed to initialize ATAPI interface!"), PANIC_INITIALIZATION_FAILED);
     panic_if(!E820_INIT(), PANIC_TEXT("Failed to initialize E820 memory map!"), PANIC_INITIALIZATION_FAILED);
     KDEBUG_PUTS("[atOS] E820 OK\n");
     panic_if(!PAGEFRAME_INIT(), PANIC_TEXT("Failed to initialize page frame! Possibly not enough memory."), PANIC_INITIALIZATION_FAILED);
+    KDEBUG_PUTS("[atOS] PAGEFRAME OK\n");
     panic_if(!KHEAP_INIT(KHEAP_MAX_SIZE_PAGES), PANIC_TEXT("Failed to initialize kheap. Not enough memory or pageframe issue."), PANIC_INITIALIZATION_FAILED);    
     KDEBUG_PUTS("[atOS] KHEAP OK\n");
     // For some reason, PAGEFRAME or something? -
     //   corrupts or zeroes the E820 entries, so we re-init it here
     panic_if(!REINIT_E820(), PANIC_TEXT("Failed to re-initialize E820!"), PANIC_INITIALIZATION_FAILED);
+    KDEBUG_PUTS("[atOS] E820 REINIT OK\n");
     panic_if(!PAGING_INIT(), PANIC_TEXT("Failed to initialize paging!"), PANIC_INITIALIZATION_FAILED);
     KDEBUG_PUTS("[atOS] PAGING OK\n");
 
@@ -52,12 +60,14 @@ void rtos_kernel(U0) {
     debug_vram_end(); // End early mode, now using task framebuffers
     init_multitasking();
     INIT_RTC();
+    KDEBUG_PUTS("[atOS] RTC init OK\n");
     STI;
 
     // No startup melody
     
     panic_if(!ATA_PIO_INIT(), PANIC_TEXT("Failed to initialize ATA PIO"), PANIC_INITIALIZATION_FAILED);
-    
+    KDEBUG_PUTS("[atOS] ATA PIO init OK\n");
+
     if (RTL8139_START()) {
         KDEBUG_PUTS("[atOS] RTL8139 started\n");
     } else {
