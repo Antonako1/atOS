0cdc398d8cad64c154d344159023c416906dee07
Antonako1 <114558368+Antonako1@users.noreply.github.com>
2026-03-07 12:18:11 +0200

    Update CLEAN.sh for make usage

--- SCRIPTS/CLEAN.sh
+++ SCRIPTS/CLEAN.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-echo "[1/2] Running make clean..."
-make clean
+if [ "$1" != "from_make" ]; then
+    echo "[1/2] Running make clean..."
+    make clean
+fi
 
 echo "[2/2] Removing all build directories..."
 find . -type d -name "build" -exec rm -rf {} +
