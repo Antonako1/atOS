a177f895b91b0707d369c7c9d4ec045c463811dc
Antonako1 <114558368+Antonako1@users.noreply.github.com>
2026-05-01 22:02:20 +0300

    pic remap end from 0x2f to 0x30

--- SOURCE/KERNEL/32RTOSKRNL/CPU/PIC/PIC.h
+++ SOURCE/KERNEL/32RTOSKRNL/CPU/PIC/PIC.h
@@ -15,7 +15,7 @@ void PIC_Mask(int irq);
 
 #define PIC_REMAP_OFFSET 0x20
 #define PIC_REMAP_OFFSET2 0x28
-#define PIC_REMAP_END     0x2F
+#define PIC_REMAP_END     0x30
 
 void pic_send_eoi(U8 irq);
 void pic_remap(U8 offset1, U8 offset2);
