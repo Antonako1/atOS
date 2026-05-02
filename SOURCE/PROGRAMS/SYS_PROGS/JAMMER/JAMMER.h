cf3fac321b7978a914b336a5e2fe1fe9b5889877
Antonako1 <114558368+Antonako1@users.noreply.github.com>
2026-05-02 23:09:03 +0300

    Create JAMMER
    Jammer, very basic wav file player

---
+++ SOURCE/PROGRAMS/SYS_PROGS/JAMMER/JAMMER.h
@@ -0,0 +1,10 @@
+#ifndef JAMMER_H
+#define JAMMER_H
+
+#include <STD/TYPEDEF.h>
+#include <LIBRARIES/ATWAV/ATWAV.h>
+#include <LIBRARIES/ATUI/ATUI.h>
+#include <STD/AUDIO.h>
+#include <STD/PROC_COM.h>
+
+#endif // JAMMER_H
